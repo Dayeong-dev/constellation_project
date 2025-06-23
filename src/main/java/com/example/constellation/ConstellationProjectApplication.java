80c5b94cc8d4e05ef93591108407cce41ba7959f
Dayeong-dev <ydiosa98@gmail.com>
2025-06-23 17:05:44 +0900

    first commit

---
+++ src/main/java/com/example/constellation/ConstellationProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.constellation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConstellationProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConstellationProjectApplication.class, args);
+	}
+
+}
